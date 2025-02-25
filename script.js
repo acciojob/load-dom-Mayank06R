84410af19765fc6f83ce11a24c933e7b6d53a1ee
Mayank06R <85189108+Mayank06R@users.noreply.github.com>
2025-02-25 19:26:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+  document.addEventListener('DOMContentLoaded', function() {
+            // Ensure body is empty
+            document.body.innerHTML = '';
+            
+            // Write "DOM load success" to the webpage
+            document.body.innerHTML = 'DOM load success';
+        });
